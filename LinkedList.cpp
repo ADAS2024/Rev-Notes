c5b94b2e5fe0f0b1ac8000c611b62dfe4a20f0bd
adas20 <dasar-24@rhodes.edu>
2025-05-17 17:27:03 -0400

    TODO: Implement methods

---
+++ LinkedList.cpp
@@ -0,0 +1,23 @@
+#include <cstring>
+#include <iostream>
+
+
+
+class DoublyLinkedList {
+	private:
+	  struct Node {
+		int data;
+		struct Node* next;
+		struct Node* prev;
+	  };
+
+	  Node* head;
+	  Node* tail;
+	
+	public:
+	  void add() {}
+	  void print() {}
+	  void remove() {}
+		
+	
+}
